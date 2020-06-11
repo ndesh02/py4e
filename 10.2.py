b59f1bf8b0f1923fea4e991e866632ea67f03169
ndesh02 <54901274+ndesh02@users.noreply.github.com>
2020-06-10 22:06:09 -0400

    Some py4e programs

---
+++ 10.2.py
@@ -0,0 +1,27 @@
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+r=0
+counts=dict()
+
+for line in handle:
+    line=line.rstrip()
+    lst=line.split()
+    try:
+        if lst[0]=='From':
+            hours=lst[5].split(':')
+            counts[hours[0]]=counts.get(hours[0],0)+1
+    except:
+        r=1
+        continue  
+
+#sort by the key
+lst=list()
+for key,val in counts.items():
+    newTup=(key,val)
+    lst.append(newTup)
+
+lst=sorted(lst)
+
+for key,val in lst:
+    print(key,val)
