bc0e86b40959ff1ee9fdc9dd0353209d89a29916
ndesh02 <54901274+ndesh02@users.noreply.github.com>
2020-06-17 18:17:47 -0400

    Learned how to do many to many relationships in databases by breaking it into one to one relationships

--- tracks/tracks.py
+++ tracks/tracks.py
@@ -97,7 +97,7 @@ for entry in all:
 #added based on website
 sqlstr = '''SELECT Track.title, Artist.name, Album.title, Genre.name
     FROM Track JOIN Genre JOIN Album JOIN Artist
-    ON Track.genre_id = Genre.ID and Track.album_id = Album.id
+    ON Track.genre_id = Genre.ID AND Track.album_id = Album.id
         AND Album.artist_id = Artist.id
     ORDER BY Artist.name LIMIT 3'''
 
